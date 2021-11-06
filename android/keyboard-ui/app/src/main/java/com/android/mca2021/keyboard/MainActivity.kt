2c27dd918a65e4d604f27c5818b4e02653bde719
Seonjun Kim <cyanide17@snu.ac.kr>
2021-11-06 18:58:27 +0900

    Add input method enable button

--- android/keyboard-ui/app/src/main/java/com/android/mca2021/keyboard/MainActivity.kt
+++ android/keyboard-ui/app/src/main/java/com/android/mca2021/keyboard/MainActivity.kt
@@ -1,16 +1,19 @@
 package com.android.mca2021.keyboard
 
 import android.Manifest
-import android.content.Context
+import android.content.*
 import android.content.pm.PackageManager
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
 import android.util.Log
+import android.view.View
 import android.view.inputmethod.InputMethodManager
+import android.widget.Button
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,6 +28,12 @@ class MainActivity : AppCompatActivity() {
             )
         }
     }
+
+    override fun onResume() {
+        super.onResume()
+        checkKeyboardSettings()
+    }
+
 
     private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
         ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
@@ -44,9 +53,58 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    fun enableKeyboard(view: android.view.View) {
+        startActivity(Intent(Settings.ACTION_INPUT_METHOD_SETTINGS))
+    }
+
     fun openKeyboardSetting(view: android.view.View) {
         val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         inputMethodManager.showInputMethodPicker()
+    }
+
+    fun checkKeyboardSettings() {
+        val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        val enabledMethods = inputMethodManager.enabledInputMethodList
+        val btn_enablekeyboard = findViewById<Button>(R.id.btn_enablekeyboard)
+        var enabled = false
+        for(i in 0 until enabledMethods.size){
+            val imi = enabledMethods[i]
+            val name = imi.loadLabel(packageManager).toString()
+            Log.d(TAG, name)
+            if(name.equals("Facemoji")){
+                enabled = true
+                btn_enablekeyboard.setBackgroundColor(Color.BLUE)
+                btn_enablekeyboard.setText("facemoji enabled! ✓")
+            }
+        }
+        if(enabled == false){
+            btn_enablekeyboard.setBackgroundColor(Color.DKGRAY)
+            btn_enablekeyboard.setText("enable facemoji")
+        }
+
+        val defaultIME = Settings.Secure.getString(this.contentResolver, Settings.Secure.DEFAULT_INPUT_METHOD)
+        val btn_selectkeyboard = findViewById<Button>(R.id.btn_selectkeyboard)
+        if(defaultIME.contains("facemoji", ignoreCase = true)) {
+            btn_selectkeyboard.setBackgroundColor(Color.BLUE)
+            btn_selectkeyboard.setText("facemoji selected! ✓")
+        }else{
+            btn_selectkeyboard.setBackgroundColor(Color.DKGRAY)
+            btn_selectkeyboard.setText("select facemoji as default")
+        }
+    }
+
+    fun checkIfDefault(view: android.view.View) {
+        val defaultIME = Settings.Secure.getString(this.contentResolver, Settings.Secure.DEFAULT_INPUT_METHOD)
+        val btn_selectkeyboard = findViewById<Button>(R.id.btn_selectkeyboard)
+        if(defaultIME.contains("facemoji", ignoreCase = true)) {
+            btn_selectkeyboard.setBackgroundColor(Color.BLUE)
+            btn_selectkeyboard.setText("facemoji selected! ✓")
+            Toast.makeText(this.applicationContext, "facemoji is set as default keyboard", Toast.LENGTH_SHORT).show()
+        }else{
+            btn_selectkeyboard.setBackgroundColor(Color.DKGRAY)
+            btn_selectkeyboard.setText("select facemoji as default")
+            Toast.makeText(this.applicationContext, "facemoji not set as default keyboard", Toast.LENGTH_SHORT).show()
+        }
     }
 
     companion object {
@@ -56,5 +114,6 @@ class MainActivity : AppCompatActivity() {
         const val REQUEST_PERMISSION = "com.android.mca2021.keyboard.requestPermission"
         val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
     }
+
 
 }
