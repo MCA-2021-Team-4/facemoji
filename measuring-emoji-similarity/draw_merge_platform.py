21a70508b90f013439c12d933e358b8d58d35512
ParkJiHo <iks15174@daum.net>
2021-12-11 22:36:46 +0900

    working on stranges ssim

--- measuring-emoji-similarity/draw_merge_platform.py
+++ measuring-emoji-similarity/draw_merge_platform.py
@@ -1,4 +1,10 @@
 from sys import argv
+import os
+import numpy as np
+import cv2
+import plotly.express as px
+from draw_table import draw_table
+
 
 platforms = ["Apple", "Facebook", "Google", "Samsung", "Twitter"]
 
@@ -28,6 +34,37 @@ def get_platform_rank(metric, plat):
                     rank += index
     return rank
 
+def get_image_pair(selected_platform, metric):
+    target_dir = os.path.join("emojis", selected_platform)
+    merged_f = open(f"merged/{metric}.txt", "r")
+    
+    
+    
+    emojis = []
+    for i, emoji in enumerate(sorted(os.listdir(target_dir))):
+        # ex) smiling_face.png
+        emojis.append(emoji)
+    
+    row = 0
+    img_result = []    
+    while True:
+        line = merged_f.readline()
+        if not line:
+            break
+        
+        listline = list(line.split())
+        listline = [int(index) for index in listline]
+        
+        row_img = cv2.imread(os.path.join(target_dir, emojis[row]))
+        for i in listline:
+            col_img = cv2.imread(os.path.join(target_dir, emojis[i]))
+            pair_img = np.hstack((row_img, col_img))
+            img_result.append(pair_img)
+            
+        row += 1
+        
+    return img_result
+
 if __name__ == "__main__":
     # if len(argv) < 2 :
     #     usage_msg = """
@@ -40,13 +77,23 @@ if __name__ == "__main__":
     metric = argv[1]
     # mark_dir = argv[2]
     
+    strange_f = open(f"strange/{metric}.txt", "r")
+    strange_line = strange_f.readline()
+    strange_line = list(strange_line.split())
+    strange_line = [int(value) for value in strange_line]
+    
     minRank = 80000
     for plat in platforms:
         rank = get_platform_rank(metric, plat)
         if rank < minRank:
             minRank = rank
             selected_platform = plat
+            
+    print(f"finish getting platfrom. selected platform is {selected_platform}")
+            
+    img_result = get_image_pair(selected_platform, metric)
+    draw_table(img_result, 8, strange_line)
     
-    print(selected_platform)
+    
     
     
